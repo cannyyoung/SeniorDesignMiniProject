45af835918519513038098853359d1df0da78d17
Wooyoung <wcho@bu.edu>
2021-09-13 04:03:57 -0400

    made the code my own, removed data and lines not necessary

--- App.js
+++ App.js
@@ -3,134 +3,62 @@ https://gist.github.com/goodpic/f1ba553d85f96c76b6b2992faf037d87
 https://blog.jscrambler.com/how-to-use-react-native-camera
 */
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet, Alert, AppRegistry, TouchableOpacity } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-class ProductScanRNCamera extends Component {
-
-  constructor(props) {
-    super(props);
-    this.camera = null;
-    this.barcodeCodes = [];
-
-    this.state = {
-      camera: {
-        type: RNCamera.Constants.Type.back,
-  flashMode: RNCamera.Constants.FlashMode.auto,
-      }
-    };
+class FoodScannerApp extends Component {
+  onBarCodeRead(scanData){
+    console.warn(scanData.data);
+  }
+  render(){
+    return(
+    <View style={styles.MyStyle}>
+        <RNCamera style = {styles.CameraStyle}
+          ref = {ref => {
+            this.camera = ref;
+          }}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          />
+          <View style={styles.MyButtonWin}>
+            <TouchableOpacity onPress={this.takePictures.bind(this)} style={styles.MyButton}>
+                <Text style={{fontSize: 12}}>Read Code</Text>
+            </TouchableOpacity>
+          </View>
+    </View>
+    )
   }
 
-  onBarCodeRead(scanResult) {
-    console.warn(scanResult.type);
-    console.warn(scanResult.data);
-    if (scanResult.data != null) {
-  if (!this.barcodeCodes.includes(scanResult.data)) {
-    this.barcodeCodes.push(scanResult.data);
-    console.warn('onBarCodeRead call');
-  }
-    }
-    return;
-  }
-
-  async takePicture() {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  }
-
-  pendingView() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'lightgreen',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>Waiting</Text>
-      </View>
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-            ref={ref => {
-              this.camera = ref;
-            }}
-            defaultTouchToFocus
-            flashMode={this.state.camera.flashMode}
-            mirrorImage={false}
-            onBarCodeRead={this.onBarCodeRead.bind(this)}
-            onFocusChanged={() => {}}
-            onZoomChanged={() => {}}
-            permissionDialogTitle={'Permission to use camera'}
-            permissionDialogMessage={'We need your permission to use your camera phone'}
-            style={styles.preview}
-            type={this.state.camera.type}
-        />
-        <View style={[styles.overlay, styles.topOverlay]}>
-    <Text style={styles.scanScreenMessage}>Please scan the barcode.</Text>
-  </View>
-  <View style={[styles.overlay, styles.bottomOverlay]}>
-          <Button
-            onPress={() => { console.log('scan clicked'); }}
-            style={styles.enterBarcodeManualButton}
-            title="Enter Barcode"
-           />
-  </View>
-      </View>
-    );
+  takePictures = async() => {
+  const options = { quality: 0.5, base64: true };
+  const data = await camera.takePictureAsync(options);
+  console.log(data.uri);
   }
 }
 
-const styles = {
-  container: {
-    flex: 1
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  overlay: {
-    position: 'absolute',
-    padding: 16,
-    right: 0,
-    left: 0,
-    alignItems: 'center'
-  },
-  topOverlay: {
-    top: 0,
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  bottomOverlay: {
-    bottom: 0,
-    backgroundColor: 'rgba(0,0,0,0.4)',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  enterBarcodeManualButton: {
-    padding: 15,
-    backgroundColor: 'white',
-    borderRadius: 40
-  },
-  scanScreenMessage: {
-    fontSize: 14,
-    color: 'white',
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-};
+const styles = StyleSheet.create({
+    MyStyle : {
+        flex : 1,
+        flexDirection : 'column',
+        backgroundColor: 'black',
+    },
+    CameraStyle : {
+        flex : 1,
+        alignItems: 'center',
+    },
+    MyButton : {
+        flex : 0,
+        backgroundColor: 'white',
+        borderRadius : 10,
+        padding: 20,
+        paddingHorizontal: 10,
+        alignSelf: 'center',
+        flexDirection : 'row',
+    },
+    MyButtonWin : {
+            flex : 0,
+            flexDirection : 'row',
+            justifyContent : 'center',
+    }
+})
 
-export default ProductScanRNCamera;
+export default FoodScannerApp
