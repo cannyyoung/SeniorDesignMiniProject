c5a1a4eaf6b5a529a45f811ed485a8efc8837cff
npatel2380 <64820257+npatel2380@users.noreply.github.com>
2021-09-18 14:20:51 -0400

    Add files via upload

---
+++ __tests__/App-api-test1.js
@@ -0,0 +1,60 @@
+//Nutrition api key for dept of agriculture: uEPEwLs6B6dcRUNu6KeYJ2iCDRRznxP0AomGKNq4
+//https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=uEPEwLs6B6dcRUNu6KeYJ2iCDRRznxP0AomGKNq4
+
+import React from "react";
+//import { Text, View } from 'react-native';
+
+import {
+    StyleSheet,
+    View,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    TouchableOpacity
+} from "react-native";
+
+
+export default class Source extends React.Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: "Source Listing",
+            headerStyle: { backgroundColor: "#fff" },
+            headerTitleStyle: { textAlign: "center", flex: 1 }
+        };
+    };
+
+    //constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            dataSource: []
+        };
+    }
+    componentDidMount() {
+
+        fetch("https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=uEPEwLs6B6dcRUNu6KeYJ2iCDRRznxP0AomGKNq4")
+            .then(response => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    loading: false,
+                    dataSource: responseJson
+                })
+            })
+            .catch(error => console.log(error)) //to catch the errors if any
+    }
+/*
+    const HelloWorldApp = () => {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Hello, world!</Text>
+            </View>
+        )
+    }
+
+    export default HelloWorldApp;*/
